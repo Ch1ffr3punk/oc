949d7865dd64abada3a3265309b28d67be48303d
Stefan Claas <sacenator@gmail.com>
2025-09-27 22:36:27 +0200

    Add files via upload

---
+++ ochome-server/ochome-server-linux.go
@@ -0,0 +1,98 @@
+package main
+
+import (
+	"crypto/rand"
+	"flag"
+	"fmt"
+	"io/ioutil"
+	"log"
+	"math/big"
+	"net/http"
+	"os"
+	"path/filepath"
+	"strings"
+	"time"
+)
+
+const (
+	alphanumeric   = "abcdefghijklmnopqrstuvwxyz0123456789"
+	filenameLength = 12
+	defaultInbox   = "inbox"
+)
+
+var (
+	inboxPath string
+	timestamp bool
+)
+
+func main() {
+	flag.StringVar(&inboxPath, "p", defaultInbox, "Path to the inbox directory")
+	flag.BoolVar(&timestamp, "t", false, "Set file timestamp to 1.1.1970 UTC +0000")
+	flag.Parse()
+
+	os.MkdirAll(inboxPath, 0755)
+	http.HandleFunc("/upload", handleUpload)
+	fmt.Printf("Server running on http://localhost:8080, inbox: %s, timestamp: %v\n", inboxPath, timestamp)
+	log.Fatal(http.ListenAndServe(":8080", nil))
+}
+
+func generateRandomFilename(length int) (string, error) {
+	b := make([]byte, length)
+	for i := range b {
+		n, _ := rand.Int(rand.Reader, big.NewInt(int64(len(alphanumeric))))
+		b[i] = alphanumeric[n.Int64()]
+	}
+	return string(b), nil
+}
+
+func handleUpload(w http.ResponseWriter, r *http.Request) {
+	defer func() {
+		time.Sleep(time.Duration(time.Now().UnixNano()%5000+1000) * time.Millisecond)
+		fmt.Fprint(w, "OK")
+	}()
+
+	if r.Method != http.MethodPost {
+		return
+	}
+
+	file, _, err := r.FormFile("file")
+	if err != nil {
+		return
+	}
+	defer file.Close()
+
+	content, _ := ioutil.ReadAll(file)
+	messageStr := string(content)
+	
+	var payload string
+	if idx := strings.Index(messageStr, "\n\n"); idx != -1 {
+		payload = messageStr[idx+2:]
+	} else {
+		payload = messageStr
+	}
+
+	savePlainTextMessage([]byte(payload))
+}
+
+func savePlainTextMessage(data []byte) {
+	filename, err := generateRandomFilename(filenameLength)
+	if err != nil {
+		filename = fmt.Sprintf("%d", time.Now().Unix())
+	}
+
+	fullPath := filepath.Join(inboxPath, filename)
+	ioutil.WriteFile(fullPath, data, 0600)
+
+	if timestamp {
+		// Einfache Lösung die auf allen Plattformen funktioniert
+		epoch := time.Unix(0, 0).UTC()
+		if err := os.Chtimes(fullPath, epoch, epoch); err != nil {
+			log.Printf("Warning: Could not set timestamp: %v", err)
+			log.Printf("Message saved as: %s", fullPath)
+		} else {
+			log.Printf("Message saved as: %s (timestamp set to 1.1.1970 UTC +0000)", fullPath)
+		}
+	} else {
+		log.Printf("Message saved as: %s", fullPath)
+	}
+}
